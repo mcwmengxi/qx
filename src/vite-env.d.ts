ce638345fdbb5c25b7b98e24f53d6a58d6082fca
mochunwang <mochunwang@qinsilk.com>
2022-10-14 13:26:15 +0800

    build: 📦️  别名配置

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,5 @@
+/// <reference path="path/types.d.ts" />
 /// <reference types="vite/client" />
-
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
