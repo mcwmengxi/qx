dd61b49c8af676e367c1a770ad6eb469320ed608
mochunwang <mochunwang@qinsilk.com>
2022-11-30 16:49:05 +0800

    feat: 错误页面处理

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,20 @@
 import HelloWorldVue from '@/components/HelloWorld.vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, Router } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
+
+/** 自动导入全部静态路由，无需再手动引入！匹配 src/router/modules 目录（任何嵌套级别）中具有 .ts 扩展名的所有文件，除了 remaining.ts 文件
+ * 如何匹配所有文件请看：https://github.com/mrmlnc/fast-glob#basic-syntax
+ * 如何排除文件请看：https://cn.vitejs.dev/guide/features.html#negative-patterns
+ */
+const modules: Record<string, any> = import.meta.glob(['./modules/**/*.ts', '!./modules/**/remaining.ts'], {
+	eager: true
+})
+/** 原始静态路由（未做任何处理） */
+const routes = []
+
+Object.keys(modules).forEach(key => {
+	routes.push(modules[key].default)
+})
 
 const constantRoutes: RouteRecordRaw[] = [
 	{
@@ -53,9 +67,9 @@ const constantRoutes: RouteRecordRaw[] = [
 	}
 ]
 
-const router = createRouter({
+const router: Router = createRouter({
 	history: createWebHashHistory(),
-	routes: constantRoutes
+	routes: constantRoutes.concat(...routes)
 })
 
 export default router
